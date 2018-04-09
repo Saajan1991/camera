df732f20d3ee11c027274682a27356792dc47347
Krestronic1 <krestronic1@Krestronic1s-Mac-mini.local>
2018-04-10 09:34:12 +1000

    camera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
-import { Camera } from 'ionic/native';
-import { Page } from 'ionic-angular';
+import { Camera } from '@ionic-native/camera';
+//import { Page } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -27,6 +27,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Camera } from 'ionic/native';
-import { Page } from 'ionic-angular';
+//import { NavController } from 'ionic-angular';
+//import { Camera } from 'ionic-native';
+import { Camera, CameraOptions} from '@ionic-native/camera';
+
+//import { Page } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -10,14 +12,51 @@ import { Page } from 'ionic-angular';
 export class HomePage {
 	
 	public base64Image: string;
-  	constructor(public navCtrl: NavController) {
+  	//constructor(public navCtrl: NavController) {
+
+	//}
+
+	constructor (private camera: Camera) {
 
 	}
 
 	takePicture(){
 		console.log("Camera Clicked");
-		//Camera.getPicture({})
+
+
+		/*
+		Camera.getPicture({
+			destinationType: Camera.DestinationType.DATA_URL,
+		        targetWidth: 1000,
+		        targetHeight: 1000
+		    }).then((imageData) => {
+		      // imageData is a base64 encoded string
+		        this.base64Image = "data:image/jpeg;base64," + imageData;
+		    }, (err) => {
+		        console.log(err);
+		});
+		*/
+
+		const options: CameraOptions = {
+
+	  quality: 100,
+	  destinationType: this.camera.DestinationType.DATA_URL,
+	  encodingType: this.camera.EncodingType.JPEG,
+	  mediaType: this.camera.MediaType.PICTURE
+	}
+	console.log("camera option");
+
+	this.camera.getPicture(options).then((imageData) => {
+	 // imageData is either a base64 encoded string or a file URI
+	 // If it's base64:
+	 //let base64Image = 'data:image/jpeg;base64,' + imageData;
+	}, (err) => {
+	 // Handle error
+	});
+
 
 	}
+
+	
 
 }

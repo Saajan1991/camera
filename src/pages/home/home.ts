8c8c7594f7d627cf69fc394942b87beac9622c16
Krestronic1 <krestronic1@Krestronic1s-Mac-mini.local>
2018-04-09 15:43:45 +1000

    camera button added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera } from 'ionic/native';
+import { Page } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	
+	public base64Image: string;
+  	constructor(public navCtrl: NavController) {
 
-  constructor(public navCtrl: NavController) {
+	}
 
-  }
+	takePicture(){
+		console.log("Camera Clicked");
+		//Camera.getPicture({})
+
+	}
 
 }
